69ffa3867c7d020bbc521bb1e6def4a88668de3a
daniel r <18-11086@usb.ve>
2024-05-22 18:28:02 -0400

    feat: download corenlp and set env variables

--- setup.py
+++ setup.py
@@ -1,30 +1,9 @@
 import sys
-from constants import SETUP_USAGE_MESSAGE, SETUP_FLAGS, SETUP_REPEATED_FLAGS
-from Exceptions import InvalidCommandLineUsage
-from utils import get_list_safe
-    
-class ComandLineArguments:
-    def __init__(self, arguments: str) -> None:
-        self.arguments = arguments
-        self.__create_argument_map()
-
-    def __create_argument_map(self) -> str:
-        argument_map = {}
-        for i in range(1, len(self.arguments[1:]), 2):
-            if self.arguments[i] in argument_map:
-                raise InvalidCommandLineUsage(SETUP_REPEATED_FLAGS)
-            
-            argument_map[self.arguments[i]] = get_list_safe(self.arguments, i+1, None)
-
-        self.argument_map = argument_map
-
-    def is_valid(self) -> bool:
-        complete_arguments = len(self.arguments[1:]) % 2 == 0 # every flag comes with a value
-        keys_are_flags = all(key[0]=='-' or key[0:2] == '--' for key in self.argument_map) 
-        flags_are_present = all(any(key == flag for flag in SETUP_FLAGS)for key in self.argument_map)
-        
-        return complete_arguments and keys_are_flags and flags_are_present
-
+from constants import SETUP_USAGE_MESSAGE, CORENLP_VERSION
+from Exceptions import InvalidCommandLineUsage, UnexistingFlag
+from ComandLineArguments import ComandLineArguments
+from utils import write_env_file
+import stanza
 
 def main() -> None:
     try:
@@ -33,8 +12,23 @@ def main() -> None:
 
         if not argument_parsers.is_valid():
             raise InvalidCommandLineUsage(SETUP_USAGE_MESSAGE)
+
+        path_to_core_nlp = argument_parsers.get_value('-d', '--dir')
+        choosen_language = argument_parsers.get_value('-l', '--language')
         
+
+        write_env_file(core_nlp_path=path_to_core_nlp,
+                       language=choosen_language)
+        
+        stanza.install_corenlp(dir=path_to_core_nlp)
+        stanza.download_corenlp_models(dir=path_to_core_nlp, 
+                                       model=choosen_language,
+                                       version=CORENLP_VERSION)
+
     except InvalidCommandLineUsage as e:
+        print(e.message)
+
+    except UnexistingFlag as e:
         print(e.message)
 
 if __name__ == "__main__":
