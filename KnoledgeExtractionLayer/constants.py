95f1fa1e84b04db7b7a566724472163597fc83b3
daniel r <18-11086@usb.ve>
2024-05-28 23:11:29 -0400

    chaurio aprende tonto

---
+++ KnoledgeExtractionLayer/constants.py
@@ -0,0 +1,10 @@
+BABELFY_HEADER = {
+    'application': 'application/json'
+}
+BABELFY_API_URL='https://babelfy.io/v1/disambiguate?'
+BABELFY_API_KEY='f9dcdcc4-2e98-4a64-bdfd-969af075e25b'
+
+SPOTLIGHT_HEADER = {
+    'accept': 'application/json'
+}
+SPOTLIGHT_API_URL='https://api.dbpedia-spotlight.org/en/annotate?'
