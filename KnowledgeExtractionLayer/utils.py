c383059d304f3f0ab559647fc5816f2771795868
daniel r <18-11086@usb.ve>
2024-05-29 23:19:41 -0400

    fix: remove unused files

--- KnowledgeExtractionLayer/utils.py
+++
@@ -1,7 +0,0 @@
-import constants
-
-def get_babelfy_url(text: str) -> str:
-    return constants.BABELFY_API_URL + f"text={text}&lang=EN&key={constants.BABELFY_API_KEY}"
-
-def get_spotlight_url(text: str) -> str:
-    return constants.SPOTLIGHT_API_URL + f"text={text}"
