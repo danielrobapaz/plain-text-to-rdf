c203735ecdca9691bdba4dda13d7ecb6c4a3d325
daniel r <18-11086@usb.ve>
2024-06-26 17:43:47 -0400

    feat: add statistics in annotation

--- execute.py
+++ execute.py
@@ -13,23 +13,10 @@ def main() -> None:
     ann = Annotate("""Michael Jordan played basket""", corenlp_dir)
 
     tokens_dict = ann.get_tokens_as_dict()
-    
-    eel = Extraction(ann.doc)
 
-    # Data Layer
-    #ann.display_tokens_and_labels()
-    #ann.display_dependency_tree()
+    print(tokens_dict)
 
-    # Knowledge Extracion Laye
-    eel.display_entities()
-    eel.display_relations(tokens_dict)
-    
-def signal_handler(sig, frame):
-    print("Corrida Terminada")
-    exit(0)
 
 
 if __name__=="__main__":
     main()
-    signal.signal(signal.SIGINT, signal_handler)
-    os.kill(os.getpid(), signal.SIGINT)
