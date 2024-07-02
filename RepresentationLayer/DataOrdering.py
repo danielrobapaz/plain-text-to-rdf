5f36cbdd018bf445ed89bc134642132d1ed89071
daniel r <18-11086@usb.ve>
2024-07-01 21:01:40 -0400

    fix: project structure

--- RepresentationLayer/DataOrdering.py
+++ RepresentationLayer/DataOrdering.py
@@ -1,16 +1,18 @@
 from KnowledgeExtractionLayer.Extraction import Extraction
-from Tripletes import Tripletes
+from . import Tripletes
 
 class DataOrdering:
     def __init__(self, extraction: Extraction) -> None:
         self.tripletes = []
 
         for r in extraction.relations:
-            self.tripletes.append(Tripletes(r['subject'],
-                                            r['relation'],
-                                            r['object']))
-            
-    def get_tripletes(self) -> list[Tripletes]:
+            self.tripletes.append(Tripletes.Tripletes(
+                                    r['subject'],
+                                    r['relation'],
+                                    r['object']))
+        
+
+    def get_tripletes(self) -> list[Tripletes.Tripletes]:
         return self.tripletes
             
         
